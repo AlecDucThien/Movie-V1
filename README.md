68ae6c87df1e409473ebf4fcaba73a5e718afc29
AlecDucThien <leducthien.work@gmail.com>
2025-03-12 17:38:52 +0700

    up

---
+++ README.md
@@ -0,0 +1,1 @@
+https://movie-v1.vercel.app/
--- src/components/MovieSearch.jsx
+++ src/components/MovieSearch.jsx
@@ -2,7 +2,7 @@ import { useContext } from "react"
 import { MovieContext } from "../context/MovieContext"
 
 const MovieSearch = ({title, data}) => {
-    const handleTrailer = useContext(MovieContext)
+  const handleTrailer = useContext(MovieContext)
   return (
     <div className='text-white p-10'>
         <h2 className="text-xl uppercase mb-4">{title}</h2>
@@ -16,7 +16,7 @@ const MovieSearch = ({title, data}) => {
                 <div className="w-full h-full relative group-hover:scale-110 transform transition-transform duration-500 ease-in-out">
                 <div className="absolute w-full h-full bg-black/40"></div>
                 <img
-                    src={`${import.meta.env.VITE_IMAGE_URL}${item.poster_path || item.backdrop_pathS}`}
+                    src={`${import.meta.env.VITE_IMAGE_URL}${item.poster_path || item.backdrop_path}`}
                     alt={item.title}
                     className="w-full h-full object-cover"
                 />
